5ede8942f654741b57c611378d83958473e3a0a5
RalonsoS <ralonso@stemdo.io>
2024-04-19 11:16:08 +0200

    Se modifican los endpoints para que estos llamen a las funciones de crud. Además se crea una dependencia de ls sessionlocal para crear una conexion independiente a la bd para cada query que se haga

--- soluciones/api/main.py
+++ soluciones/api/main.py
@@ -1,25 +1,53 @@
-from fastapi import FastAPI
-from datetime import date
-from models import Persona
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException
+from schema import Persona
+from sqlalchemy.orm import Session
+
+import model, schema, crud
+from db import SessionLocal
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated [Session, Depends(get_db)]
+
 @app.get("/personas")
-async def get_personas():
-    return {"message": "Hello, world"}
+async def read_personas(db: Session = Depends(get_db)):
+    db_personas = crud.get_personas(db)
+    return db_personas
 
 @app.get("/personas/{persona_id}")
-async def get_persona(persona_id: int):
-    return {"persona_id": persona_id}
+async def read_persona(persona_id: int, db: Session = Depends(get_db)):
+    db_persona = crud.get_persona(db, persona_id)
+    if db_persona is None:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return db_persona
 
-@app.post("/personas/")
-async def create_persona(persona: Persona):
-    return persona
+
+@app.post("/personas")
+async def create_persona(persona: schema.PersonaCreate, db: Session = Depends(get_db)):
+    db_persona = crud.create_persona(db, persona)
+    if db_persona is None:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return db_persona
 
 @app.put("/personas/{persona_id}")
-async def update_persona(persona_id: int):
-    return {"persona_id": persona_id}
+async def update_persona(persona_id: int, persona_update: schema.PersonaUpdate, db: Session = Depends(get_db)):
+    db_persona = crud.update_persona(db, persona_id, persona_update)
+    if db_persona is None:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return db_persona
 
 @app.delete("/personas/{persona_id}")
-async def delete_persona(persona_id: int):
-    return {"persona_id": persona_id}
+def delete_persona(persona_id: int, db: Session = Depends(get_db)):
+    db_persona = crud.delete_persona(db, persona_id)
+    if db_persona is None:
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+    return db_persona
